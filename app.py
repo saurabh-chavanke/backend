f0aad27bde48f346b245ff6fe204d38f15b48353
Saurabh Chavanke <113685246+saurabh-chavanke@users.noreply.github.com>
2025-02-13 13:24:22 +0530

    port issue solve

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def process_image():
     return jsonify({"steps": processed_steps, "hemorrhage_count": hemorrhage_count})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
